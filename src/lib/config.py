4d21f73cc0d7dc0e67bf66f6f954be42c5b6a80c
Omer Priel <omer.priel2@gmail.com>
2023-02-21 16:24:17 +0200

    fix

--- src/lib/config.py
+++ src/lib/config.py
@@ -15,7 +15,7 @@ class _config:
     FILE_PATH_MAX_LENGTH = 256
 
     SOCKET_TIMEOUT: int = 3
-    SOCKET_MAXSIZE: int = 1024
+    SOCKET_MAXSIZE: int = 4096
     LOGGING_LEVEL: int = logging.DEBUG
 
 
