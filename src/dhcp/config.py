0b73ab3d3482cd7d7e608f8215e724b076b51a82
Omer Priel <omer.priel2@gmail.com>
2023-03-02 20:31:59 +0200

    add .env to dhcp

--- src/dhcp/config.py
+++ src/dhcp/config.py
@@ -1,6 +1,7 @@
 # config of the DHCP server
 
 import os
+from dotenv import load_dotenv
 
 from src.lib.config import Config
 from src.lib.config import init_logging as base_init_logging
@@ -17,7 +18,15 @@ config = AppConfig()
 
 
 def init_config() -> None:
-    pass
+    if os.path.isfile("src/dhcp/.env"):
+        load_dotenv("src/dhcp/.env")
+
+    if os.getenv("SERVER_PORT"):
+        config.SERVER_PORT = int(os.getenv("SERVER_PORT"))
+    if os.getenv("CLIENT_PORT"):
+        config.CLIENT_PORT = int(os.getenv("CLIENT_PORT"))
+    if os.getenv("DATABASE_PATH"):
+        config.DATABASE_PATH = os.getenv("DATABASE_PATH")
 
 
 def init_logging() -> None:
